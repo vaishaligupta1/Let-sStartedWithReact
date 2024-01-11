ade77a2e96948fa10827123ef53f1c961d6c076d
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2024-01-11 17:44:56 +0530

    Making Components Smaller and Readable Task 10

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,12 @@
+import './ExpenseItems.css'
+function ExpenseDetails(props){
+    const { title, amount, location } = props;
+    return(
+        <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div>{location}</div>
+        <div className="expense-item__price">${amount}</div>
+        </div>
+    )
+}
+export default ExpenseDetails
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css' ;
-
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props)
 {
     // const expenseDate = new Date(2023,1,9);
     // const expenseTitle = "Car Insurance";
     // const expenseAmount = 294.47;
     //const LocationOfExpenditure = "Mumbai";
+    
     return (
     <div className ="expense-item">
-        <div>{props.date.toISOString()}</div>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <h2>{props.location}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-        </div>
+        <ExpenseDate date ={props.date}/>
+        <ExpenseDetails title ={props.title} location={props.location} amount ={props.amount}></ExpenseDetails>
     </div>
     )
 }
