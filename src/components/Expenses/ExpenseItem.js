ddfd88eda780508d711977efbd41498caa113252
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2024-01-23 14:14:24 +0530

    Creating Price updation Button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import Card from '../UI/Card';
 const ExpenseItem=(props)=>
 {   
     const [title, setTitle]=useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
 
     const clickHandler = ()=>{
         setTitle("Updated!");
@@ -14,15 +15,20 @@ const ExpenseItem=(props)=>
     const deleteHandler =()=>{
         console.log("delete ho gaya")
     }
+    const priceHandler =()=>{
+        setAmount("$100");
+        console.log("clicked!!!!")
+    }
     return (   
     <Card className ="expense-item">
         <ExpenseDate date ={props.date}/>
         <div className="expense-item__description">
         <h2>{title}</h2>
         <h3>{props.location}</h3>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
+        <button onClick={priceHandler}>Change Price</button>
         <button onClick={deleteHandler}>Delete Expense</button>
     </Card>
     )
